191aeddce980f804505fb29a607421e3fe45c079
keanedawg <cfife@chicagoventure.com>
2017-07-27 20:13:58 -0500

    knob is now one-way binded to variable called doggy

--- app/components/cams-knob.js
+++ app/components/cams-knob.js
@@ -2,7 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     initializeComponent: function() {
-        this.$().knob();
-    }.on('didInsertElement')
+        var self = this;
+        this.$().knob({
+            change: function(v) {
+                self.set('value', v);
+            }
+        });
+    }.on('didInsertElement'),
+
+    valueChanged: function() {
+        
+    }.observes('value')
+
 
 });
