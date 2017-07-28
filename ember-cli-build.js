5c5b4eb3c6d7bf081cc17789daaf74e91d6c5015
keanedawg <cfife@chicagoventure.com>
2017-07-27 19:06:32 -0500

    added jquerry knob

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,6 +8,8 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('bower_components/jquery-knob/js/jquery.knob.js');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
